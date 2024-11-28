1fb2d3a37f052c9ef09c2d9ebdc59bc165c4aa8a
Pesala winodith <gkpesalawinodith@gmail.com>
2024-11-28 16:32:53 +0530

    import methods and use them

---
+++ main.py
@@ -0,0 +1,8 @@
+import my_project.my_calculator.addition;
+import my_project.my_calculator.subtraction;
+
+result1 = my_project.my_calculator.addition.add(5,3)
+result2 = my_project.my_calculator.subtraction.subtract(8,6)
+
+print("Addition result",result1)
+print("Subtraction result",result2)
