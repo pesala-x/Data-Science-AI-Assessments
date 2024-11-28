c1ba43861a7a73bc641cf4b1f8967a5cf22c6d7e
Pesala winodith <gkpesalawinodith@gmail.com>
2024-11-28 16:55:54 +0530

    location updated

--- my_project/main.py
+++ my_project/main.py
@@ -1,10 +1,9 @@
-from my_project.my_calculator.addition import add
-from my_project.my_calculator.subtraction import subtract 
-
-
+from my_calculator.addition import add
+from my_calculator.subtraction import subtract 
 
 result1 = add(5,3)
 result2 = subtract(8,6)
 
 print("Addition result",result1)
 print("Subtraction result",result2)
+ 
