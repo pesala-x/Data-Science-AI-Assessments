279bd036d6a9a2f8a551be96f8fbb2ac420c86ae
Pesala winodith <gkpesalawinodith@gmail.com>
2024-12-12 09:40:42 +0530

    test the readlines() method

--- day6/file_handling.py
+++ day6/file_handling.py
@@ -1,7 +1,9 @@
 # file1 = open("day6\my_file.txt")
 file1 = open("day6\my_file.txt", "r")
 
-content = file1.readline()
+# content = file1.read()
+# content = file1.readline()
+content = file1.readlines()
 
 print(content,type(content))
 
