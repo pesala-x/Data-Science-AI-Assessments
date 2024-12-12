ab3e6c2227d33776a8a364388f745d3a02e29ad9
Pesala winodith <gkpesalawinodith@gmail.com>
2024-12-12 11:55:19 +0530

    get values in content using key values

--- json_file_handling/file_handler.py
+++ json_file_handling/file_handler.py
@@ -7,6 +7,9 @@ json_file_path = "json_file_handling/example_1.json"
 with open(json_file_path,"r")as json_file:
     # load the json data into a python dictionary
     content = json.load(json_file)
-
+    
 # contains the content of the JSON file print as the dictionary
 print(content)
+
+for key,value in content.items():
+    print(key,value)
