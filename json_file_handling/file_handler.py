253f06e2729a86f164f89cfbb17d27e0148ed81f
Pesala winodith <gkpesalawinodith@gmail.com>
2024-12-12 11:46:32 +0530

    python JSON type file handling tested with created json file content reading

---
+++ json_file_handling/file_handler.py
@@ -0,0 +1,12 @@
+import json;
+
+# specify the path to JSON file
+json_file_path = "json_file_handling/example_1.json"
+
+# open the JSON file for reading
+with open(json_file_path,"r")as json_file:
+    # load the json data into a python dictionary
+    content = json.load(json_file)
+
+# contains the content of the JSON file print as the dictionary
+print(content)
